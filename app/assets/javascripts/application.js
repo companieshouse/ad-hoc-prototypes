0e120388ff007a5e2a73c7b6c55ac3d37edf3527
James Francis <jfranciswebdesign@users.noreply.github.com>
2025-09-08 10:09:55 +0100

    Updated prototype kit version, added OC auth code screens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,20 @@
-/* global $ */
+//
+// For guidance on how to add JavaScript see:
+// https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
+//
 
-// Warn about using the kit in production
-if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
-}
+window.GOVUKPrototypeKit.documentReady(() => {
 
-$(document).ready(function () {
-  window.GOVUKFrontend.initAll()
+
+// Prevents 'dummy' hyperlinks (those with href="#") from reloading/jumping to the top of the page
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href="#"]').forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+  });
+});
+
+
+
 })
